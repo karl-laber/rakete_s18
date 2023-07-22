838e7d6907870374943f4049c08952bfdd8e5baa
karl-laber <61885980+karl-laber@users.noreply.github.com>
2023-07-22 16:05:30 +0200

    RGB_LED S 21

--- main.ts
+++ main.ts
@@ -1,54 +1,6 @@
-function Start_Rakete () {
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        . # # # .
-        # # # # #
-        # . # . #
-        `)
-    basic.showLeds(`
-        . # # # .
-        . # # # .
-        # # # # #
-        # . # . #
-        . . . . .
-        `)
-    basic.showLeds(`
-        . # # # .
-        # # # # #
-        # . # . #
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # # # # #
-        # . # . #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # . # . #
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.clearScreen()
-}
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    Countdown()
-    Start_Rakete()
+basic.forever(function () {
+    basic.setLedColor(0xff0000)
+    basic.pause(1000)
+    basic.turnRgbLedOff()
+    basic.pause(1000)
 })
-function Countdown () {
-    basic.showNumber(3)
-    basic.pause(Tempo)
-    basic.showNumber(2)
-    basic.pause(Tempo)
-    basic.showNumber(1)
-    basic.pause(Tempo)
-    basic.showNumber(0)
-    basic.pause(Tempo)
-}
-let Tempo = 0
-Tempo = 1000
